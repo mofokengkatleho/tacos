db2c1a3f836149081f2f867cd40173b7ba1cdcf1
katleho <katlehomofokeng@protonmail.com>
2022-12-15 23:14:52 +0200

    TacoOrder fields

--- src/main/java/katleho/tacos/model/TacoOrder.java
+++ src/main/java/katleho/tacos/model/TacoOrder.java
@@ -2,6 +2,18 @@ package katleho.tacos.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+    private List<Taco> tacos = new ArrayList<>();
 }
