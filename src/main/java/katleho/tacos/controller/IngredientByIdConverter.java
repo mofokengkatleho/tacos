75ee0825eb2d22f4f13edb0ac15efb90d8c6bffa
katleho <katlehomofokeng@protonmail.com>
2022-12-16 11:00:33 +0200

    IngredientByIdConverter implements Converter interface

--- src/main/java/katleho/tacos/controller/IngredientByIdConverter.java
+++ src/main/java/katleho/tacos/controller/IngredientByIdConverter.java
@@ -1,4 +1,12 @@
 package katleho.tacos.controller;
 
-public class IngredientByIdConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientByIdConverter  implements Converter<String,String> {
+    @Override
+    public String convert(String source) {
+        return null;
+    }
 }
