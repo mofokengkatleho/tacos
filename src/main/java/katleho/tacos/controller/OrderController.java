b67a1532570462c9ef68f66360ccca07f5aa8cbb
katleho <katlehomofokeng@protonmail.com>
2022-12-16 11:24:30 +0200

    PostMapping processOrder in OrderController

--- src/main/java/katleho/tacos/controller/OrderController.java
+++ src/main/java/katleho/tacos/controller/OrderController.java
@@ -1,10 +1,13 @@
 package katleho.tacos.controller;
 
+import katleho.tacos.model.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/orders")
@@ -14,6 +17,13 @@ public class OrderController {
     @GetMapping
     public String orderForm(){
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, SessionStatus status){
+        log.info("Order submitted: {}",tacoOrder);
+        status.setComplete();
+        return "redirect:/";
     }
 
 }
