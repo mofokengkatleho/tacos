2e2fa55d28ff973df397afc457850f2cbda382ad
katleho <katlehomofokeng@protonmail.com>
2022-12-16 11:18:30 +0200

    implemented OrderController

--- src/main/java/katleho/tacos/controller/OrderController.java
+++ src/main/java/katleho/tacos/controller/OrderController.java
@@ -1,4 +1,19 @@
 package katleho.tacos.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+@Slf4j
 public class OrderController {
+    @GetMapping
+    public String orderForm(){
+        return "orderForm";
+    }
+
 }
