22f2c56513582007ae0042233a53ee1b9eb0712e
katleho <katlehomofokeng@protonmail.com>
2022-12-19 23:48:29 +0200

    mapRowToIngredient helper method in IngredientRepositoryImpl

--- src/main/java/katleho/tacos/repository/IngredientRepositoryImpl.java
+++ src/main/java/katleho/tacos/repository/IngredientRepositoryImpl.java
@@ -1,10 +1,21 @@
 package katleho.tacos.repository;
 
 import katleho.tacos.model.Ingredient;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
+@Repository
 public class IngredientRepositoryImpl implements IngredientRepository{
+    private JdbcTemplate jdbcTemplate;
+
+    public IngredientRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Iterable<Ingredient> findAll() {
         return null;
@@ -18,5 +29,12 @@ public class IngredientRepositoryImpl implements IngredientRepository{
     @Override
     public Ingredient save(Ingredient ingredient) {
         return null;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type")));
     }
 }
