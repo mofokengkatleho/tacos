2d98818499c7d3f8cdef6507a8792523d9a75f9b
katleho <katlehomofokeng@protonmail.com>
2022-12-19 23:28:08 +0200

    id and timestamp fields for TacoOrder class

--- src/main/java/katleho/tacos/model/TacoOrder.java
+++ src/main/java/katleho/tacos/model/TacoOrder.java
@@ -5,11 +5,16 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+    private static final long serialVersionUID = 1l;
+    private long id;
+    private Date placedAt;
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
     @NotBlank(message = "Street is required")
