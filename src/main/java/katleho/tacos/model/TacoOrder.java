d81941a0eb24362a94b6c579fe812793963a1efb
katleho <katlehomofokeng@protonmail.com>
2022-12-19 06:28:45 +0200

    Validation for CVV and ccExpiration field inm TacoOrder class

--- src/main/java/katleho/tacos/model/TacoOrder.java
+++ src/main/java/katleho/tacos/model/TacoOrder.java
@@ -2,6 +2,8 @@ package katleho.tacos.model;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
@@ -23,7 +25,9 @@ public class TacoOrder {
     private String ccNumber;
     @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco){
