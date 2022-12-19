4f330452b92f0a09f56b3d732628d86a66b4f886
katleho <katlehomofokeng@protonmail.com>
2022-12-19 06:23:46 +0200

    validation for fields in TacoOrder class

--- src/main/java/katleho/tacos/model/TacoOrder.java
+++ src/main/java/katleho/tacos/model/TacoOrder.java
@@ -1,7 +1,7 @@
 package katleho.tacos.model;
 
 import lombok.Data;
-
+import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +14,11 @@ public class TacoOrder {
     private String deliveryStreet;
     @NotBlank(message = "City is required")
     private String deliveryCity;
+    @NotBlank(message = "State is required")
     private String deliveryState;
+    @NotBlank(message = "ZIP code is required")
     private String deliveryZip;
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
