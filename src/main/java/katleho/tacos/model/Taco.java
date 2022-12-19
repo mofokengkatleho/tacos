189a8b79c3844b6d2af5c8acf177183644fad758
katleho <katlehomofokeng@protonmail.com>
2022-12-19 23:24:18 +0200

    add id and dateCreated field in Taco class

--- src/main/java/katleho/tacos/model/Taco.java
+++ src/main/java/katleho/tacos/model/Taco.java
@@ -5,10 +5,13 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+    private Long id;
+    private Date dateCreated = new Date();
     @NotNull
     @Size(min=5,message="Name must be at least 5 characters long")
     String name;
