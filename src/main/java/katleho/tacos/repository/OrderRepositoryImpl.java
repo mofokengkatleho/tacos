9060661e132332cdd8b0415a7e1a12a9943cb5f7
katleho <katlehomofokeng@protonmail.com>
2022-12-20 01:59:52 +0200

    OrderRepository intial commit

---
+++ src/main/java/katleho/tacos/repository/OrderRepositoryImpl.java
@@ -0,0 +1,10 @@
+package katleho.tacos.repository;
+
+import katleho.tacos.model.TacoOrder;
+
+public class OrderRepositoryImpl implements OrderRepository{
+    @Override
+    public TacoOrder save(TacoOrder tacoOrder) {
+        return null;
+    }
+}
