91b5192dd744fc6f363b152b224de95894423b9d
katleho <katlehomofokeng@protonmail.com>
2022-12-19 06:13:50 +0200

    Validation for name field in Taco class

--- src/main/java/katleho/tacos/model/Taco.java
+++ src/main/java/katleho/tacos/model/Taco.java
@@ -1,12 +1,16 @@
 package katleho.tacos.model;
 
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min=5,message="Name must be at least 5 characters long")
     String name;
     private List<Ingredient> ingredients;
 }
