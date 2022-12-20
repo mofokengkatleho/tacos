3477e0c483f03f7f9ad9532d248cf4c6277a3ffa
katleho <katlehomofokeng@protonmail.com>
2022-12-20 19:21:02 +0200

    created UserRepository

---
+++ src/main/java/katleho/tacos/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package katleho.tacos.repository;
+
+import katleho.tacos.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByUsername(String username);
+}
