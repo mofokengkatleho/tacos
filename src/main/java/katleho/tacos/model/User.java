0a75a90103be123d86482a64ae03b1b74e5326d4
katleho <katlehomofokeng@protonmail.com>
2022-12-20 19:09:09 +0200

    edit User class

--- src/main/java/katleho/tacos/model/User.java
+++ src/main/java/katleho/tacos/model/User.java
@@ -7,13 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -27,7 +26,7 @@ public class User implements UserDetails {
 
     private final String username;
     private final String password;
-    private final String fullname;
+    private final String fullName;
     private final String street;
     private final String city;
     private final String state;
@@ -36,7 +35,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
