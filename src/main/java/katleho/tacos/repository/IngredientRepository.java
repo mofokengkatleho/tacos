5ad9d65b94bea0f3860f6cd9c02e62fdb90ce65c
katleho <katlehomofokeng@protonmail.com>
2022-12-20 17:36:56 +0200

    Spring Security dependency

--- src/main/java/katleho/tacos/repository/IngredientRepository.java
+++ src/main/java/katleho/tacos/repository/IngredientRepository.java
@@ -2,7 +2,6 @@ package katleho.tacos.repository;
 
 import katleho.tacos.model.Ingredient;
 import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
 
 public interface IngredientRepository extends CrudRepository<Ingredient,String> {
 }
