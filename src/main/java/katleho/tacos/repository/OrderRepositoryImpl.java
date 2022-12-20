259969f1253c39377efe009d8cbe85164e035934
katleho <katlehomofokeng@protonmail.com>
2022-12-20 02:01:51 +0200

    OrderRepository constructor

--- src/main/java/katleho/tacos/repository/OrderRepositoryImpl.java
+++ src/main/java/katleho/tacos/repository/OrderRepositoryImpl.java
@@ -1,8 +1,15 @@
 package katleho.tacos.repository;
 
 import katleho.tacos.model.TacoOrder;
+import org.springframework.jdbc.core.JdbcOperations;
 
 public class OrderRepositoryImpl implements OrderRepository{
+    private JdbcOperations jdbcOperations;
+
+    public OrderRepositoryImpl(JdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
     @Override
     public TacoOrder save(TacoOrder tacoOrder) {
         return null;
