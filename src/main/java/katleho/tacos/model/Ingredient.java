2375b98e9cfda6c2e631642cf36cf7045212ae1f
katleho <katlehomofokeng@protonmail.com>
2022-12-20 09:16:42 +0200

    Ingredient -> Entity

--- src/main/java/katleho/tacos/model/Ingredient.java
+++ src/main/java/katleho/tacos/model/Ingredient.java
@@ -5,23 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
+
+
+import javax.persistence.Entity;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient  implements Persistable<String> {
+public class Ingredient {
     @Id
     private final String id;
     private final String name;
     private final Type type;
 
-    @Override
-    public boolean isNew() {
-        return false;
-    }
 
     public enum Type{
         WRAP,SAUCE,CHEESE,VEGGIES,PROTEIN
