978c36f8f5b368e84d50a4c7f1e5e200504ae407
katleho <katlehomofokeng@protonmail.com>
2022-12-20 03:17:31 +0200

    annotate Ingredient class and fields

--- src/main/java/katleho/tacos/model/Ingredient.java
+++ src/main/java/katleho/tacos/model/Ingredient.java
@@ -1,9 +1,18 @@
 package katleho.tacos.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-public class Ingredient {
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Ingredient  {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/katleho/tacos/model/TacoOrder.java
+++ src/main/java/katleho/tacos/model/TacoOrder.java
@@ -2,6 +2,9 @@ package katleho.tacos.model;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -11,8 +14,10 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1l;
+    @Id
     private long id;
     private Date placedAt;
     @NotBlank(message = "Delivery name is required")
