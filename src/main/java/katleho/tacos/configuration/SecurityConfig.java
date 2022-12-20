627073a670b6110002c8a760899787111dd8acdf
katleho <katlehomofokeng@protonmail.com>
2022-12-20 19:30:07 +0200

    created UserDetailsService class

--- src/main/java/katleho/tacos/configuration/SecurityConfig.java
+++ src/main/java/katleho/tacos/configuration/SecurityConfig.java
@@ -1,7 +1,11 @@
 package katleho.tacos.configuration;
 
+import katleho.tacos.model.User;
+import katleho.tacos.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +14,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if(user != null) return user;
+            throw new UsernameNotFoundException("User '"+ username+"' not found");
+        };
     }
 }
