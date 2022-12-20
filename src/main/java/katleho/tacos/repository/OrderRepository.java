1d5f4b93726d338f0375376af3ec14765fe7d85d
katleho <katlehomofokeng@protonmail.com>
2022-12-20 03:03:28 +0200

    OrderRepository extends CrudRepository

--- src/main/java/katleho/tacos/repository/OrderRepository.java
+++ src/main/java/katleho/tacos/repository/OrderRepository.java
@@ -1,7 +1,8 @@
 package katleho.tacos.repository;
 
 import katleho.tacos.model.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder,Long> {
     TacoOrder save(TacoOrder tacoOrder);
 }
