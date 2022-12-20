cf79332c7d6f5893a5e8c88d99b652e22059c2b4
katleho <katlehomofokeng@protonmail.com>
2022-12-20 02:08:44 +0200

    first commit of save method in OrderRepositoryImpl

--- src/main/java/katleho/tacos/repository/OrderRepositoryImpl.java
+++ src/main/java/katleho/tacos/repository/OrderRepositoryImpl.java
@@ -1,7 +1,17 @@
 package katleho.tacos.repository;
 
+import katleho.tacos.model.Taco;
 import katleho.tacos.model.TacoOrder;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class OrderRepositoryImpl implements OrderRepository{
     private JdbcOperations jdbcOperations;
@@ -11,7 +21,46 @@ public class OrderRepositoryImpl implements OrderRepository{
     }
 
     @Override
+    @Transactional
     public TacoOrder save(TacoOrder tacoOrder) {
-        return null;
+        PreparedStatementCreatorFactory preparedStatementCreatorFactory =
+                new PreparedStatementCreatorFactory(
+                        "insert into Taco_Order "
+                                + "(delivery_name, delivery_street, delivery_city, "
+                                + "delivery_state, delivery_zip, cc_number, "
+                                + "cc_expiration, cc_cvv, placed_at) "
+                                + "values (?,?,?,?,?,?,?,?,?)",
+                        Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                        Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                        Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
+                );
+        preparedStatementCreatorFactory.setReturnGeneratedKeys(true);
+
+        tacoOrder.setPlacedAt(new Date());
+        PreparedStatementCreator psc =
+                preparedStatementCreatorFactory.newPreparedStatementCreator(
+                        Arrays.asList(
+                                tacoOrder.getDeliveryName(),
+                                tacoOrder.getDeliveryStreet(),
+                                tacoOrder.getDeliveryCity(),
+                                tacoOrder.getDeliveryState(),
+                                tacoOrder.getDeliveryZip(),
+                                tacoOrder.getCcNumber(),
+                                tacoOrder.getCcExpiration(),
+                                tacoOrder.getCcCVV(),
+                                tacoOrder.getPlacedAt()));
+
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcOperations.update(psc, keyHolder);
+        long orderId = keyHolder.getKey().longValue();
+        tacoOrder.setId(orderId);
+
+        List<Taco> tacos = tacoOrder.getTacos();
+        int i=0;
+        for (Taco taco : tacos) {
+            saveTaco(orderId, i++, taco);
+        }
+        return tacoOrder;
     }
+    private long saveTaco(Long orderId, int orderKey, Taco taco) {
 }
