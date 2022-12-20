842738067b5f1e5b24fd3e45191ce633656a0548
katleho <katlehomofokeng@protonmail.com>
2022-12-20 03:04:01 +0200

    remove save method in OrderRepository

--- src/main/java/katleho/tacos/repository/OrderRepository.java
+++ src/main/java/katleho/tacos/repository/OrderRepository.java
@@ -4,5 +4,4 @@ import katleho.tacos.model.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<TacoOrder,Long> {
-    TacoOrder save(TacoOrder tacoOrder);
 }
