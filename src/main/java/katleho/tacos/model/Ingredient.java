68ca28b47c0297b98f9b5f3a25fb35db66cea1d6
katleho <katlehomofokeng@protonmail.com>
2022-12-20 03:22:39 +0200

    annotate models

--- src/main/java/katleho/tacos/model/Ingredient.java
+++ src/main/java/katleho/tacos/model/Ingredient.java
@@ -5,17 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient  {
+public class Ingredient  implements Persistable<String> {
     @Id
     private final String id;
     private final String name;
     private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
 
     public enum Type{
         WRAP,SAUCE,CHEESE,VEGGIES,PROTEIN
