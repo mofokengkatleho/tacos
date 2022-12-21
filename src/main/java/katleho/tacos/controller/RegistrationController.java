c5106d475183591a0c1e312509feb5d8f560cb4c
katleho <katlehomofokeng@protonmail.com>
2022-12-21 19:43:04 +0200

    autowired UserRepository to RegistrationController class

--- src/main/java/katleho/tacos/controller/RegistrationController.java
+++ src/main/java/katleho/tacos/controller/RegistrationController.java
@@ -1,9 +1,18 @@
 package katleho.tacos.controller;
 
+import katleho.tacos.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("register")
 public class RegistrationController {
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
