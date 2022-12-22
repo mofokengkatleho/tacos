92a2cb0f0c6de6c41ed9b206910cd0d9127d32e2
katleho <katlehomofokeng@protonmail.com>
2022-12-22 16:22:56 +0200

    Autowire userRepo in RegistrationController

--- src/main/java/katleho/tacos/controller/RegistrationController.java
+++ src/main/java/katleho/tacos/controller/RegistrationController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("register")
+@RequestMapping("/register")
 public class RegistrationController {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
