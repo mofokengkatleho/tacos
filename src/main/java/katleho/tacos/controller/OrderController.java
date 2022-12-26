5eeee1f6089676eb832e8ebdb725a26b5c4dc654
katleho <katlehomofokeng@protonmail.com>
2022-12-26 18:24:33 +0200

    edit orderForm method in OrderController

--- src/main/java/katleho/tacos/controller/OrderController.java
+++ src/main/java/katleho/tacos/controller/OrderController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
@@ -26,8 +23,24 @@ public class OrderController {
         this.orderRepository = orderRepository;
     }
 
-    @GetMapping
-    public String orderForm(){
+    @GetMapping("/current")
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute TacoOrder tacoOrder) {
+        if (tacoOrder.getDeliveryName() == null)
+            tacoOrder.setDeliveryName(user.getFullname());
+
+        if (tacoOrder.getDeliveryStreet() == null)
+            tacoOrder.setDeliveryStreet(user.getStreet());
+
+        if (tacoOrder.getDeliveryCity() == null)
+            tacoOrder.setDeliveryCity(user.getCity());
+
+        if (tacoOrder.getDeliveryState() == null)
+            tacoOrder.setDeliveryState(user.getState());
+
+        if (tacoOrder.getDeliveryZip() == null)
+            tacoOrder.setDeliveryZip(user.getZip());
+
         return "orderForm";
     }
 
